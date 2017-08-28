fdcda744c1a41644e28fcec8b6ff252bf660d277
梁世茂 <liangshimao@bacic5i5j.com>
2017-08-28 11:09:28 +0800

    first commit

---
+++ time.php
@@ -0,0 +1,36 @@
+<?php
+
+
+/**
+ * 时间转为为几天前,几分钟前等数据
+ * @param $the_time
+ * @return string
+ */
+function time_tran($the_time) {
+    $now_time = date("Y-m-d H:i:s", time());
+    //echo $now_time;
+    $now_time = strtotime($now_time);
+    $show_time = strtotime($the_time);
+    $dur = $now_time - $show_time;
+    if ($dur < 0) {
+        return date('Y-m-d H:i', strtotime($the_time));
+    } else {
+        if ($dur < 60) {
+            return $dur . '秒前';
+        } else {
+            if ($dur < 3600) {
+                return floor($dur / 60) . '分钟前';
+            } else {
+                if ($dur < 86400) {
+                    return floor($dur / 3600) . '小时前';
+                } else {
+                    if ($dur < 259200) {//3天内
+                        return floor($dur / 86400) . '天前';
+                    } else {
+                        return date('Y-m-d', strtotime($the_time));
+                    }
+                }
+            }
+        }
+    }
+}
